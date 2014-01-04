8be45ca50fe215e67ddd7cf3bf6d06b245c803e2
David Creemer <david@zachary.com>
2014-01-04 15:06:04 -0800

    add bootstrap hint

--- bin/init/go.sh
+++ bin/init/go.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 #
 # bootstrap config
-#
+# init a new machine with "bash <(curl -fsSL https://raw.github.com/dcreemer/dotfiles/master/bin/init/go.sh)"
 # can be safely re-run
+
 
 if [ ! -r $HOME/.dotfiles ]; then
   echo "Checking out dotfiles..."
