c4d404ebba84d28ba307a00e2891b05fde897c5c
David Creemer <david@zachary.com>
2014-01-04 21:47:37 -0800

    add priv and work dotfiles targets

--- bin/init/go.sh
+++ bin/init/go.sh
@@ -1,19 +1,31 @@
 #! /bin/bash
 #
-# bootstrap config
+# (re-)bootstrap an environment
 # init a new machine with "bash <(curl -fsSL https://raw.github.com/dcreemer/dotfiles/master/bin/init/go.sh)"
 # can be safely re-run
 
+echo "[START]"
 
 if [ ! -r $HOME/.dotfiles ]; then
-  echo "Checking out dotfiles..."
+  echo "[CLONE] dotfiles"
   git clone git://github.com/dcreemer/dotfiles.git $HOME/.dotfiles
 fi
 
-if [ ! -d $HOME/bin ]; then
-    mkdir $HOME/bin
+$HOME/.dotfiles/bin/link-dotfiles base
+
+# fetch git-remote-gcrypt to bootstrap private repos.
+grc="$HOME/bin/git-remote-gcrypt"
+if [ ! -r $grc ]; then
+    echo "[FETCH] $git-remote-gcrypt"
+    curl -fsSL "https://raw.github.com/dcreemer/git-remote-gcrypt/master/git-remote-gcrypt" > $grc
+    chmod a+x $grc
 fi
 
-$HOME/.dotfiles/bin/link-dotfiles
+if [ -d $HOME/.dotfiles-private ]; then
+    $HOME/.dotfiles/bin/link-dotfiles priv
+fi
+if [ -d $HOME/.dotfiles-work ]; then
+    $HOME/.dotfiles/bin/link-dotfiles work
+fi
 
-echo "done."
+echo "[DONE]"
