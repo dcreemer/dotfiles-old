abafef6adf38899646b6359596d91fae515c1ca3
David Creemer <david@zachary.com>
2014-01-04 23:42:28 -0800

    cleanup and remove copy/paste code

--- bin/init/go.sh
+++ bin/init/go.sh
@@ -4,6 +4,31 @@
 # init a new machine with "bash <(curl -fsSL https://raw.github.com/dcreemer/dotfiles/master/bin/init/go.sh)"
 # can be safely re-run
 
+do_install()
+{
+    # run pre-install hook, install and run post-install hook
+    # for the given param
+    m=$1
+    DF=$HOME/.dotfiles-${m}
+    
+    if [ -d $DF ]; then
+        
+        # run pre-install hook
+        if [ -r ${DF}/pre-install.sh ]; then
+            ${DF}/pre-install.sh
+        fi
+
+        # install
+        $HOME/.dotfiles-base/bin/link-dotfiles $m
+
+        # run post-install hook
+        if [ -r ${DF}/post-install.sh ]; then
+            ${DF}/post-install.sh
+        fi
+        
+    fi
+}
+
 echo "[START]"
 
 # fetch dotfiles-base
@@ -12,42 +37,21 @@ if [ ! -r $HOME/.dotfiles-base ]; then
   git clone git://github.com/dcreemer/dotfiles.git $HOME/.dotfiles-base
 fi
 
-# run pre-install hooks
-if [ -r $HOME/.dotfiles-base/pre-install.sh ]; then
-    $HOME/.dotfiles-base/pre-install.sh
-fi
-
 # install
-$HOME/.dotfiles-base/bin/link-dotfiles base
+do_install base
 
-# run post-install hooks
-if [ -r $HOME/.dotfiles-base/pre-install.sh ]; then
-    $HOME/.dotfiles-base/pre-install.sh
-fi
-
-# possibly clone private and work bootstraps:
-if [ -d $HOME/Dropbox/Git ]; then
-    for m in private work ; do
-        REPO=$HOME/Dropbox/Git/dotfiles-${m}.git
-        if [ ! -r $HOME/.dotfiles-${m} ] && [ -d $REPO ]; then
-            echo "[CLONE] dotfiles-${m}"
-            git clone gcrpyt::rsync:${REPO} $HOME/.dotfiles-{m}
-        fi
-    done
-fi
+# if available, clone and install private and work bootstraps
 
 for m in private work ; do
-    # run pre-install hook
-    if [ -r $HOME/.dotfiles-${m}/pre-install.sh ]; then
-        $HOME/.dotfiles-${m}/pre-install.sh
-    fi
-    if [ -d $HOME/.dotfiles-${m} ]; then
-        # link files
-        $HOME/.dotfiles-base/bin/link-dotfiles $m
-        # run post-install hook
-        if [ -r $HOME/.dotfiles-${m}/post-install.sh ]; then
-            $HOME/.dotfiles-${m}/post-install.sh
-        fi
+    # check Dropbox for encrypted repositories
+    REPO=$HOME/Dropbox/Git/dotfiles-${m}.git
+    if [ ! -r $HOME/.dotfiles-${m} ] && [ -d $REPO ]; then
+        # clone:
+        echo "[CLONE] dotfiles-${m}"
+        git clone gcrpyt::rsync:${REPO} $HOME/.dotfiles-${m}
+
+        # install:
+        do_install $m
     fi
 done
 
