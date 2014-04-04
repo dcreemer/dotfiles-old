e3cefa51d9cce47ebfb499027bfeb703a6f421f0
David Creemer <david@zachary.com>
2014-04-03 21:39:06 -0700

    update lein profile

--- dot/.lein/profiles.clj
+++ dot/.lein/profiles.clj
@@ -3,12 +3,12 @@
 {:user {:dependencies [[org.clojure/tools.namespace "0.2.4"]
                        [clj-time "0.6.0"]
                        [spyscope "0.1.4" :exclusions [clj-time]]
-                       [criterium "0.4.2"]]
+                       [criterium "0.4.3"]]
         :injections [(require '(clojure.tools.namespace repl find))
                      ; try/catch to workaround an issue where `lein repl` outside a project dir
                      ; will not load reader literal definitions correctly:
                      (try (require 'spyscope.core)
                           (catch RuntimeException e))]
         :plugins [[lein-pprint "1.1.1"]
-                  [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
-                  [lein-ancient "0.5.4" :exclusions [commons-codec clj-time]]]}}
+                  [lein-ancient "0.5.5"]
+                  [lein-localrepo "0.5.3"]]}}
