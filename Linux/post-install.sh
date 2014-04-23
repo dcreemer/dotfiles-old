12068fb0295fe6e7d238d6ce435c84343af8e8fa
David Creemer <david@zachary.com>
2014-04-23 15:50:06 -0700

    fix typo

--- Linux/post-install.sh
+++ Linux/post-install.sh
@@ -8,7 +8,7 @@ set -o errexit
 # fetch git-remote-gcrypt to bootstrap private repos.
 grc="$HOME/bin/git-remote-gcrypt"
 if [ ! -r $grc ]; then
-    echo "[FETCH] $git-remote-gcrypt"
+    echo "[FETCH] git-remote-gcrypt"
     curl -fsSL "https://raw.github.com/dcreemer/git-remote-gcrypt/master/git-remote-gcrypt" > $grc
     chmod a+x $grc
 fi
