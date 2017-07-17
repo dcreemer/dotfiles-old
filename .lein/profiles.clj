10f194c04ccfded25a64a20e6873fd878a8ac71c
David Creemer <david@zachary.com>
2017-07-17 10:59:43 -0700

    update to recent

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,15 +1,15 @@
 ; based on https://gist.github.com/jamesmacaulay/5603176
 ;
 {:user {:dependencies [[org.clojure/tools.namespace "0.2.10"]
-                       [org.clojure/tools.nrepl "0.2.12"]
-                       [clj-time "0.11.0"]
-                       [spyscope "0.1.5" :exclusions [clj-time]]
+                       [org.clojure/tools.nrepl "0.2.13"]
+                       [clj-time "0.13.0"]
+                       [spyscope "0.1.6" :exclusions [clj-time]]
                        [criterium "0.4.4"]]
         :injections [; try/catch to workaround an issue where `lein repl` outside a project dir
                      ; will not load reader literal definitions correctly:
                      (try (require 'spyscope.core)
                           (catch RuntimeException e))]
         :plugins [[lein-ancient "0.6.10"]
-                  [lein-localrepo "0.5.3"]
-                  [lein-midje "3.2"]
-                  [lein-kibit "0.1.2"]]}}
+                  [lein-localrepo "0.5.4"]
+                  [lein-midje "3.2.1"]
+                  [lein-kibit "0.1.5"]]}}
