60850fcf8685039ba654f37c463a0c24cf4adc54
David Creemer <david@zachary.com>
2019-03-10 14:54:20 -0700

    add update script; adjust default apps

---
+++ bin/morning.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+# my morning ritual
+
+function banner() {
+    echo ""
+    echo "======================"
+    echo "$1"
+    echo "======================"
+    echo ""
+}
+
+banner "App Store"
+mas upgrade
+
+banner "Rust"
+rustup update
+
+banner "Brew"
+brew update && brew outdated
+echo "-> 'brew upgrade'"
+
+banner "Brew Cask"
+brew cask outdated
+echo "-> 'brew cask upgrade'"
