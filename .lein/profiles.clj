6de1edead9c9f8313c08b66d6adee93e4cc7d1b9
David Creemer <david@zachary.com>
2019-06-11 17:13:54 -0700

    upgrade lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,7 @@
 ;; DZC lein repl defaults
 ;;
-{:user {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                       [org.clojure/tools.nrepl "0.2.13"]]
-        :plugins [[lein-ancient "0.6.14"]
+{:user {:dependencies [[org.clojure/tools.namespace "0.3.0"]]
+        :plugins [[lein-ancient "0.6.15"]
                   [lein-localrepo "0.5.4"]
                   [lein-midje "3.2.1"]
                   [lein-kibit "0.1.6"]]}}
