6695825d0825a5bbf32d290bbc88029da54d3d9f
David Creemer <david@zachary.com>
2020-05-24 12:10:00 -0700

    lint

--- .bash.d/java.sh
+++ .bash.d/java.sh
@@ -6,7 +6,7 @@
 if [ -x "/usr/libexec/java_home" ]; then
     # function that makes switching easy i.e. setjdk 1.8
     setjdk() {
-        export JAVA_HOME=$(/usr/libexec/java_home -v $1)
+        export JAVA_HOME=$(/usr/libexec/java_home -v "$1")
     }
     setjdk 11
 fi
