2ff19330c3286128e07a5950f67f8747b2bfd8dc
David Creemer <david@zachary.com>
2021-10-07 20:56:21 -0700

    fix pyenv init

--- .bash.d/python.sh
+++ .bash.d/python.sh
@@ -5,5 +5,7 @@
 
 if command -v pyenv 1>/dev/null 2>&1; then
     # use pyenv to choose and switch python interpreters
-    eval "$(pyenv init -)"
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
 fi
